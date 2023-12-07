e0a96a831e2d505795abfe9b57cd7361ca0700ff
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2023-12-07 13:41:28 -0500

    Sheet cleaned

--- prayertimings_uploader.py
+++ prayertimings_uploader.py
@@ -9,10 +9,22 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 def upload_monthly_data(excel_path, sheet_name):
-    # Read the specific sheet of the Excel file into a pandas dataframe
-    df = pd.read_excel(excel_path, sheet_name=sheet_name, header=0)
+    df = pd.read_excel(excel_path, sheet_name=sheet_name)
 
-    # Set the column names
+    # Debugging: Print the first few rows to check the structure and headers
+    print(df.head())
+
+    # Assuming 'Day' is the first column after the headers are correctly set
+    # Try accessing the 'Day' column, if it fails, print an error message
+    try:
+        df['Day'] = pd.to_numeric(df['Day'], errors='coerce')  # Convert non-numeric to NaN
+        df = df.dropna(subset=['Day'])  # Drop rows with NaN in 'Day' column
+        df['Day'] = df['Day'].astype(int)  # Convert 'Day' to integer
+    except KeyError:
+        print("The 'Day' column could not be found in the DataFrame. Please check the Excel file format.")
+        return
+
+    # Set the column names (assumes no duplicate column names and all required columns are present)
     df.columns = ["Day", 
                   "Fajr", "Sunrise", "Dhuhr", "Asr", 
                   "Magrib", "Isha",
@@ -28,11 +40,10 @@ def upload_monthly_data(excel_path, sheet_name):
     # Batch write operation
     for record in data_dict:
         day = str(record['Day'])  # Convert day to string to use as document ID
-       # Set the document in Firestore using the day as the document ID
+        # Set the document in Firestore using the day as the document ID
         month_ref.document(sheet_name).collection('Days').document(day).set(record)
 
-
-    print(f"Data from sheet '{sheet_name}' uploaded to Firestore under 'PrayerTimings/{sheet_name}/Days/{day}'")
+    print(f"Data from sheet '{sheet_name}' uploaded to Firestore under 'PrayerTimings/{sheet_name}/Days'")
 
 # Usage
 excel_file_path = 'yearly_timetable.xlsx'
