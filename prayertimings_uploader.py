20a8c43a9f807d1de27a99eb25f9d6fe3a0413f3
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2023-12-07 13:29:23 -0500

    update

--- prayertimings_uploader.py
+++ prayertimings_uploader.py
@@ -12,34 +12,27 @@ def upload_monthly_data(excel_path, sheet_name):
     # Read the specific sheet of the Excel file into a pandas dataframe
     df = pd.read_excel(excel_path, sheet_name=sheet_name, header=0)
 
-    # Assuming the first row contains the column headers, we'll reassign them just to be sure
+    # Set the column names
     df.columns = ["Day", 
                   "Fajr", "Sunrise", "Dhuhr", "Asr", 
                   "Magrib", "Isha",
                   "Fajr Iqamah", "Dhuhr Iqamah", "Asr Iqamah", 
                   "Magrib Iqamah", "Isha Iqamah"]
 
-    # Remove any rows where 'Day' is not a digit (just in case there are any such rows)
-    df = df[df['Day'].apply(lambda x: str(x).isdigit())]
-
     # Convert the dataframe to a dictionary
     data_dict = df.to_dict(orient='records')
 
-    # Reference to Firestore document for the month
-    month_ref = db.collection('PrayerTimings').document(sheet_name)
+    # Reference to Firestore collection for the month
+    month_ref = db.collection('PrayerTimings')
 
     # Batch write operation
     for record in data_dict:
-        # Check if 'Day' is a digit and convert it to a string
-        if str(record['Day']).isdigit():
-            day = str(int(record['Day']))  # Convert day to string
-            # Create a document reference for the day number
-            day_ref = month_ref.collection(day)
+        day = str(record['Day'])  # Convert day to string to use as document ID
+       # Set the document in Firestore using the day as the document ID
+        month_ref.document(sheet_name).collection('Days').document(day).set(record)
 
-            # Add a document with a predefined ID, such as 'timings'
-            day_ref.document(record).set(record)
 
-    print(f"Data from sheet '{sheet_name}' uploaded to Firestore under 'PrayerTimings/{sheet_name}'")
+    print(f"Data from sheet '{sheet_name}' uploaded to Firestore under 'PrayerTimings/{sheet_name}/Days/{day}'")
 
 # Usage
 excel_file_path = 'yearly_timetable.xlsx'
